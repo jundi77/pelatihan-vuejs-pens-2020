3ade573788164af6a5302e0a55bf4a6c8f73983b
Jundullah H. R <jundihanif77@gmail.com>
2020-08-08 23:25:17 +0700

    Bug hunting

--- js/main.js
+++ js/main.js
@@ -14,33 +14,13 @@ let app = new Vue({
 		subKeyword: util.debounce(async function subKeywordHandler()
 		{
 			this.clearResult()
-			if(this.vidLinkInput == this.vidDesc.title) {
-				this.vidLinkInput = this.vidLink
-			}
-			if(this.vidLink != this.vidLinkInput) {
-				this.vidDesc = ''
-			}
-			this.vidLink = this.vidLinkInput
-			if(this.vidLinkInput != '') {
-				fetch('https://yt-title-find.000webhostapp.com/?url=' + encodeURIComponent(this.vidLinkInput))
-					.then(response => response.json())
-					.then(data => {
-						this.vidDesc = data
-						console.log(data)
-					})
-					.then(() => {
-						if(this.vidDesc.title != undefined)
-							this.vidLinkInput = this.vidDesc.title
-					})
-					.catch((error) => {
-						console.error('Error:', error);
-						this.error = true
-					})
+			if(this.vidLinkInput != this.vidDesc.title)
+					this.vidLink = this.vidLinkInput
+			if(this.vidLink != '') {
 				if(this.vidLink.length > 0 && this.subKeyword.length >= 3) {
-					fetch('https://cari-teks-video-api.vercel.app/api/search?url=' + encodeURIComponent(this.vidLinkInput) + '&q=' + this.subKeyword)
+					fetch('https://cari-teks-video-api.vercel.app/api/search?url=' + encodeURIComponent(this.vidLink) + '&q=' + this.subKeyword)
 						.then(response => response.json())
 						.then(data => {
-							console.log(data)
 							this.foundList.push(data)
 							if(data.total > 0) 
 								this.flattenResult(data)
@@ -52,6 +32,25 @@ let app = new Vue({
 				}
 			}
 		}
+		,250),
+		vidLinkInput: util.debounce(async function findVideoTitle()
+			{
+				if(this.vidLinkInput != this.vidDesc.title)
+					this.vidLink = this.vidLinkInput
+				fetch('https://yt-title-find.000webhostapp.com/?url=' + encodeURIComponent(this.vidLink))
+					.then(response => response.json())
+					.then(data => {
+						this.vidDesc = data
+					})
+					.then(() => {
+						if(this.vidDesc.title != undefined)
+							this.vidLinkInput = this.vidDesc.title
+					})
+					.catch((error) => {
+						console.error('Error:', error);
+						this.error = true
+					})
+			}
 		,250)
 	},
 	methods: {
@@ -78,18 +77,15 @@ let app = new Vue({
 		},
 		loadingResultVisible(isVisible, entry)
 		{
-			console.log({isVisible,entry})
 			this.loadingResult = isVisible
 			if(isVisible) this.loadResult()
 		},
 		loadResult ()
 		{
 			if(this.foundList.length > 0 && this.foundList[0].total > 0 && this.foundList[this.foundList.length-1].next != null){
-				console.log('load')
 				fetch(this.foundList[this.foundList.length-1].next)
 					.then(response => response.json())
 					.then(data => {
-						console.log(data)
 						this.foundList.push(data)
 						if(data.total > 0) 
 							this.flattenResult(data)
@@ -110,39 +106,13 @@ let app = new Vue({
 			}
 
 		},
-		searchSubAndVidDesc()
-		{
-			fetch('https://yt-title-find.000webhostapp.com/?url=' + encodeURIComponent(this.vidLinkInput))
-				.then(response => response.json())
-				.then(data => {
-					this.vidDesc = data
-					console.log(data)
-				})
-			this.vidLink = this.vidLinkInput
-			if(this.vidLink.length > 0 && this.subKeyword.length >= 3) {
-				fetch('https://cari-teks-video-api.vercel.app/api/search?url=' + encodeURIComponent(this.vidLink) + '&q=' + this.subKeyword)
-					.then(response => response.json())
-					.then(data => {
-						console.log(data)
-						this.foundList.push(data)
-						if(data.total > 0) 
-							this.flattenResult(data)
-					})
-					.catch((error) => {
-						console.error('Error:', error);
-						this.error = true
-					})
-			}
-		},
 		setToVidLink(event)
 		{
-			console.log([this.vidDesc.title, this.vidLink, this.vidLinkInput])
 			event.target.value = this.vidLink
 		},
 		resetToTitleOrURL(event)
 		{
-			console.log([this.vidDesc.title, this.vidLink, this.vidLinkInput])
-			if(this.vidDesc != '') event.target.value = this.vidDesc.title
+			if(this.vidDesc != '' && this.vidDesc.title != undefined) event.target.value = this.vidDesc.title
 			else if(this.vidLink != '') event.target.value = this.vidLink
 			else event.target.value = this.vidLinkInput
 		},
